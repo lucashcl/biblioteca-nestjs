e68a93d27f922526136c79a474dd744b41ea4cf4
Lucas Cipriano <lucasc.lima26@gmail.com>
2026-02-23 14:44:06 -0300

    feat: add TypeOrm import in ReadersModule

--- src/readers/readers.module.ts
+++ src/readers/readers.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReadersService } from './readers.service';
 import { ReadersController } from './readers.controller';
+import { Reader } from './entities/reader.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Reader])],
   controllers: [ReadersController],
   providers: [ReadersService],
 })
