4614e8545e1a6eb68106f9fd781754961f792395
Lucas Cipriano <lucasc.lima26@gmail.com>
2026-03-10 14:57:06 -0300

    feat: setup openapi docs with scalar

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Biblioteca API')
+    .setDescription('API para gerenciamento de biblioteca')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  app.use(
+    '/docs',
+    apiReference({
+      content: document,
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
